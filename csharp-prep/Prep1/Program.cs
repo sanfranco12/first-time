f08c5c179d995c982a64cba2c5f15d8599c52752
Sanfranco12 <sanfranco12@gmail.com>
2024-09-19 20:48:04 -0500

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("");
+        
+        Console.Write("What is your First Name? ");
+        string first_name = Console.ReadLine();
+
+
+        Console.Write("What is your Last Name? ");
+        string last_name = Console.ReadLine();
+        Console.Write("");
+
+        Console.Write($"Your name is {last_name}, {first_name} {last_name}. ");
     }
 }
