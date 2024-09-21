77e3efb758f6aafc498a9ee3936b44059b84cece
Sanfranco12 <sanfranco12@gmail.com>
2024-09-20 19:57:29 -0500

    C# Loops Part 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the Magic Number? ");
+        string magicNumber = Console.ReadLine();
+        int number = int.Parse(magicNumber);
+
+        Console.Write("What's your guess? ");
+        string userGuess = Console.ReadLine();
+        int guess = int.Parse(userGuess);
+
+        if (guess < number)
+        {
+            Console.WriteLine("Go Higher ");
+        }
+        else if (guess > number)
+        {
+            Console.WriteLine("Go Lower ");
+        }
+        else
+        {
+            Console.WriteLine("You Guessed it! ");
+        }
     }
 }
