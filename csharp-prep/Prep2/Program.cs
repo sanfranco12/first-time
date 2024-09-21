44f34455b4742a7feda924e09531d06c46651fb6
Sanfranco12 <sanfranco12@gmail.com>
2024-09-20 19:03:05 -0500

    C# 2 done and I had to add that End Project as well, I was saving the repository in google drive, that was the issue, now is saved on local C disk, no issues.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        int gradeA = 90; 
+        int gradeB = 80;
+        int gradeC = 70;
+        int gradeD = 60;
+        int gradeF = 60;
+
+        string won = "Congratulations! you have passed!";
+        string lose = "We encourage you to keep working and try again,";
+        string letter = "";
+        Console.WriteLine("");
+        Console.WriteLine("");
+
+        Console.Write("What was your Final Grade percentage? -Please type only the number:  ");
+        string finalUserGradeInput = Console.ReadLine();
+        int finalUserGradeNumber = int.Parse(finalUserGradeInput);
+
+        if (finalUserGradeNumber >= gradeA)
+        {
+            letter = "A";
+            Console.WriteLine($"{won} your Final Grade is {letter}!!!!!!!! ");
+        }
+        else if (finalUserGradeNumber >= gradeB)
+        {
+            letter = "B";
+            Console.WriteLine($"{won} your Final Grade is {letter}! ");
+        }
+        else if (finalUserGradeNumber >= gradeC)
+        {
+            letter = "C";
+            Console.WriteLine($"{won} your Final Grade is {letter}! ");
+        }
+        else if (finalUserGradeNumber >= gradeD)
+        {
+            letter = "D";
+            Console.WriteLine($"{won} your Final Grade is {letter}! ");
+        }
+        else
+        {
+            letter = "F";
+            Console.WriteLine($"{lose} your Final Grade is {letter}! ");
+        }
+        Console.WriteLine("");
     }
 }
