77c0755b41719ae5b5d4c322ba4990def2759be5
Sanfranco12 <sanfranco12@gmail.com>
2024-09-21 20:04:01 -0500

    Functions done, pay attention to the square function and do excercises about it.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+        DisplayResult(name, square);
     }
-}
+
+    static void DisplayWelcomeMessage ()
+    {
+        Console.WriteLine("Welcome to the program!! ");
+    }
+
+    static string PromptUserName ()
+    {
+        Console.Write ("Please enter your name: ");
+        string userName = Console.ReadLine ();
+        return userName;
+    }
+    
+    static int PromptUserNumber ()
+    {
+        Console.Write ("Please enter your favorite number: ");
+        string userNumber = Console.ReadLine();
+        int number = int.Parse (userNumber);
+        return number;
+    }
+
+    static int SquareNumber (int number)
+    { 
+        int userSquareNumber = number * number;
+        return userSquareNumber; 
+    }
+
+    static void DisplayResult (string PromptUserName, int SquareNumber)
+    {
+        Console.WriteLine(PromptUserName + ", the square of your number is: " + SquareNumber);
+    }
+
+
+}   
